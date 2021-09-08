5f7637610dabb9161791b05ec6e1971f32474eb9
Leonardo Diaz <diazleonardojavier@gmail.com>
2021-09-08 14:06:03 -0300

    add comment

--- src/main/java/payapal/Perfection.java
+++ src/main/java/payapal/Perfection.java
@@ -1,3 +1,15 @@
+/**
+ This class determine if a number is perfection or not.
+
+Definition of a Perfect Number:
+In mathematics a perfect number is defined as an integer which is the sum of its proper
+positive divisors; that is, the sum of the positive divisors not including the number itself.
+Some examples of perfect numbers are:
+
+6 = 1 + 2 + 3
+28 = 1 + 2 + 4 + 7 + 14
+496 = 1 + 2 + 4 + 8 + 16 + 31 + 62 + 124 + 248
+**/
 package main.java.payapal;
 
 public class Perfection {
